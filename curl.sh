2512e4ef77e4fb7da7f75df8c1fd8d55d2dbd950
wpsadi <wpsadi@outlook.com>
2026-06-08 18:20:44 +0530

    updated runner

--- curl.sh
+++ curl.sh
@@ -1,9 +1,10 @@
-curl -X POST http://127.0.0.1:8000/query \
-  -H "Content-Type: application/json" \
-  -d '{"query": "What is the goal of this plan?"}'
+# curl -X POST http://127.0.0.1:8000/query \
+#   -H "Content-Type: application/json" \
+#   -d '{"query": "What is the goal of this plan?"}'
 
-curl -X POST http://127.0.0.1:8000/query   -H "Content-Type: application/json"   -d '{"query": "What is the goal of this plan?"}'
+# curl -X POST http://127.0.0.1:8000/query   -H "Content-Type: application/json"   -d '{"query": "What is the goal of this plan?"}'
 
 curl -X POST http://127.0.0.1:8000/ingest \
-  -F "file=@F:/vector-store/PLAN.md" \
+  -H Authorization:"Bearer qwerty-asdf" \
+  -F "file=@./PLAN.md" \
   -F "title=Plan"
