44aea54b4f9b1a5aa50cbae0dc454dd207528b54
wpsadi <wpsadi@outlook.com>
2026-06-12 12:55:50 +0530

    pacth

--- src/retrieval/__init__.py
+++ src/retrieval/__init__.py
@@ -1,13 +1,11 @@
 from src.retrieval.dense.retriever import DenseRetriever
 from src.retrieval.hybrid.hybrid_retriever import HybridRetriever
 from src.retrieval.hybrid.fusion import reciprocal_rank_fusion
-from src.retrieval.kg.retriever import KGRetriever
 from src.retrieval.sparse.retriever import SparseRetriever
 
 __all__ = [
     "DenseRetriever",
     "SparseRetriever",
     "HybridRetriever",
-    "KGRetriever",
     "reciprocal_rank_fusion",
 ]
